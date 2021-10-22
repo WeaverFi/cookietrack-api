e474712c4e59348e0ff2b3e14e2cd0b9114f6f1b
Ncookie <ncukiert@gmail.com>
2021-10-22 00:01:24 -0300

    Formatted Firebase Project

--- README.md
+++ README.md
@@ -5,17 +5,20 @@ The API that powers CookieTrack.
 
 ---
 
-[![Netlify Status](https://api.netlify.com/api/v1/badges/b1d81e7d-19e4-4326-a2db-5bd62540c58f/deploy-status)](https://app.netlify.com/sites/compassionate-roentgen-43e54a/deploys)
-
----
-
 ## Endpoints
 - **/avalanche/wallet?address=\<wallet_address\>** - Returns a wallet's token balances on the Avalanche Chain.
 
 ---
 
-To run the project locally:
+## Local Development:
+
+Uncomment line 26 on `functions/index.js`.
+
 ```
+npm i
+cd functions
+npm i
+cd..
 npm start
 ```
 
---
+++ functions/index.js
@@ -0,0 +1,29 @@
+
+// Required Packages:
+const functions = require('firebase-functions');
+const express = require('express');
+
+// Required Routes:
+const wallet = require('../routes/wallet.js');
+
+// Initializing Express Server:
+const app = express();
+
+// Default Endpoint:
+app.get('/', (req, res) => {
+  res.send(`
+    <title>CookieTrack API</title>
+    <p>Click <a href="https://github.com/Ncookiez/cookietrack-api">here</a> to see the docs and endpoints available.</p>
+  `);
+});
+
+// Avalanche Wallet Balance:
+app.get('/avalanche/wallet', async (req, res) => {
+  res.end(await wallet.get(req));
+});
+
+// Starting Local Server:
+// app.listen(3000, () => { console.log('API Up on 127.0.0.1:3000'); });
+
+// Exporting Express App:
+exports.app = functions.https.onRequest(app);
