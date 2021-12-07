0033751094adf5afafade380fec98099b4659801
Ncookie <ncukiert@gmail.com>
2021-12-07 14:19:47 -0300

    Commented out testing line

--- functions/index.js
+++ functions/index.js
@@ -132,4 +132,4 @@ app.get('/snowball/deposits', async (req, res) => {
 // app.listen(3000, () => { console.log('\nAPI Up on 127.0.0.1:3000'); });
 
 // Exporting Express App:
-exports.app = functions.runWith({ memory: '256MB', timeoutSeconds: 120 }).https.onRequest(app);
+exports.app = functions.runWith({ memory: '512MB', timeoutSeconds: 120 }).https.onRequest(app);
